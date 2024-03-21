986ae92b9cefdc233043e796e41d22f58692f568
BobWritesCode <warwick_hart@hotmail.com>
2024-03-21 00:00:23 +0000

    Create start logic of timer countdown

--- html/script.js
+++ html/script.js
@@ -20,6 +20,31 @@ const UpdateDamage = (health) => {
   DebugLog('UpdateDamage()', health);
 };
 
+const CountDown = (startTime) => {
+  const count = setInterval(function () {
+    if (startTime == 0) {
+      clearInterval(count);
+    }
+    const s = String(startTime % 60).padStart(2, '0');
+    const m = Minutes(startTime)
+    document.getElementById("M1").textContent= m[0]
+    document.getElementById("M2").textContent= m[1]
+    document.getElementById("S1").textContent= s[0]
+    document.getElementById("S2").textContent= s[1]
+    startTime--;
+  }, 1000);
+};
+
+const Minutes = (startTime) => {
+  const x = Math.floor(startTime / 60);
+  if (x > 99) {
+    return String(99).padStart(2, '0');
+  }
+  return String(x).padStart(2, '0');
+};
+
+CountDown(137);
+
 function DebugLog(str = null, _str2 = null) {
   if (!isDebug) {
     return;
