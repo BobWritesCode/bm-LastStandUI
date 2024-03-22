5420333d7e577478a39d360e1716aae887c1a199
BobWritesCode <warwick_hart@hotmail.com>
2024-03-22 10:42:55 +0000

    Add func OpenUI

--- client/main.lua
+++ client/main.lua
@@ -4,4 +4,11 @@ AddEventHandler('onResourceStart', function(resourceName)
   if GetCurrentResourceName() ~= resourceName then return end
 end)
 
+function OpenUI()
+  SendNUIMessage({
+    action = 'OpenUI',
+    timer = Config.LastStandTime
+  })
+end
+
 print("^4[Log] ^3bm-LastStandUI ^2started^7")
